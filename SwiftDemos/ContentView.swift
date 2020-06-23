73f4927ecfed54cb831225d6ec091c953b86ec64
boo <boogoogle@163.com>
2020-06-23 10:35:44 +0800

    Initial Commit

---
+++ SwiftDemos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftDemos
+//
+//  Created by Boo on 6/23/20.
+//  Copyright © 2020 boo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
